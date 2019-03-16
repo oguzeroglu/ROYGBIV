9f5cc60494d1dfac07a52549436cf20831ae115d
oguz <eroglu.oguz93@gmail.com>
2019-03-16 10:23:34 +0100

    PhysicsBodyGenerator impl

---
+++ js/handler/PhysicsBodyGenerator.js
@@ -0,0 +1,77 @@
+var PhysicsBodyGenerator = function(){
+
+}
+
+PhysicsBodyGenerator.prototype.generateBoxBody = function(params){
+  var physicsShapeKey = "BOX" + PIPE + params["x"] + PIPE + params["y"] + PIPE + params["z"];
+  var surfacePhysicsShape = physicsShapeCache[physicsShapeKey];
+  if (!surfacePhysicsShape){
+    surfacePhysicsShape = new CANNON.Box(new CANNON.Vec3(params["x"], params["y"], params["z"]));
+    physicsShapeCache[physicsShapeKey] = surfacePhysicsShape;
+  }
+  var physicsMaterial = new CANNON.Material();
+  var surfacePhysicsBody = new CANNON.Body({mass: 0, shape: surfacePhysicsShape, material: physicsMaterial});
+  return surfacePhysicsBody;
+}
+
+PhysicsBodyGenerator.prototype.generateCylinderBody = function(params){
+  var physicsShapeKey = "CYLINDER" + PIPE + params.topRadius + PIPE + params.bottomRadius + PIPE + Math.abs(params.height) + PIPE + 8 + PIPE + params.axis + PIPE + (params.height > 0);
+  var cylinderPhysicsShape = physicsShapeCache[physicsShapeKey];
+  var cached = false;
+  if (!cylinderPhysicsShape){
+      cylinderPhysicsShape = new CANNON.Cylinder(params.topRadius, params.bottomRadius, Math.abs(params.height), 8);
+      physicsShapeCache[physicsShapeKey] = cylinderPhysicsShape;
+  }else{
+    cached = true;
+  }
+  cylinderPhysicsShape.topRadius = params.topRadius;
+  cylinderPhysicsShape.bottomRadius = params.bottomRadius;
+  cylinderPhysicsShape.height = Math.abs(params.height);
+  if (params.axis == "XZ"){
+    if (!cached){
+      var quat = new CANNON.Quaternion();
+      var coef = 1;
+      if (params.height < 0){
+        coef = -1;
+      }
+      quat.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI/2 * coef);
+      var translation = new CANNON.Vec3(0, 0, 0);
+      cylinderPhysicsShape.transformAllPoints(translation, quat);
+    }
+  }else if (params.axis == "XY"){
+    if (!cached){
+      if (params.height < 0){
+        var quat = new CANNON.Quaternion();
+        quat.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), -Math.PI);
+        var translation = new CANNON.Vec3(0, 0, 0);
+        cylinderPhysicsShape.transformAllPoints(translation, quat);
+      }
+    }
+  }else if (params.axis == "YZ"){
+    if (!cached){
+      var quat = new CANNON.Quaternion();
+      var coef = 1;
+      if (params.height < 0){
+        coef = -1;
+      }
+      quat.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), coef * Math.PI/2);
+      var translation = new CANNON.Vec3(0, 0, 0);
+      cylinderPhysicsShape.transformAllPoints(translation, quat);
+    }
+  }
+  var physicsMaterial = new CANNON.Material();
+  var cylinderPhysicsBody = new CANNON.Body({mass: 0, shape: cylinderPhysicsShape, material: physicsMaterial});
+  return cylinderPhysicsBody;
+}
+
+PhysicsBodyGenerator.prototype.generateSphereBody = function(params){
+  var physicsShapeKey = "SPHERE" + PIPE + params.radius;
+  var spherePhysicsShape = physicsShapeCache[physicsShapeKey];
+  if (!spherePhysicsShape){
+    spherePhysicsShape = new CANNON.Sphere(Math.abs(params.radius));
+    physicsShapeCache[physicsShapeKey] = spherePhysicsShape;
+  }
+  var physicsMaterial = new CANNON.Material();
+  var spherePhysicsBody = new CANNON.Body({mass: 0, shape: spherePhysicsShape, material: physicsMaterial});
+  return spherePhysicsBody;
+}
