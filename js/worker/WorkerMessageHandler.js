aa86967d39de5de7341fc118c4ae1b14c9da3f43
oguz <eroglu.oguz93@gmail.com>
2019-03-17 16:00:24 +0100

    workerMessageUnder preallocate buffer

--- js/worker/WorkerMessageHandler.js
+++ js/worker/WorkerMessageHandler.js
@@ -2,20 +2,33 @@ var WorkerMessageHandler = function(worker){
   if (worker){
     this.worker = worker;
   }
-  this.buffer = [];
+  this.bufferIndex = 0;
+  this.elementCount = 0;
+  this.buffer = new Array(5);
+  this.preallocatedArrayCache = new Map();
 }
 
 WorkerMessageHandler.prototype.push = function(data){
-  this.buffer.push(data);
+  this.buffer[this.bufferIndex ++] = data;
+  this.elementCount ++;
 }
 
 WorkerMessageHandler.prototype.flush = function(){
-  if (this.buffer.length > 0){
-    if (this.worker){
-      this.worker.postMessage(this.buffer);
-    }else{
-      postMessage(this.buffer);
+  if (this.elementCount > 0){
+    var ary = this.preallocatedArrayCache.get(this.elementCount);
+    if (!ary){
+      ary = new Array(this.elementCount);
+      this.preallocatedArrayCache.set(this.elementCount, ary);
     }
-    this.buffer.length = 0;
+    for (var i = 0; i<this.elementCount; i++){
+      ary[i] = this.buffer[i];
+    }
+    if (this.worker){
+      this.worker.postMessage(ary);
+    }else{
+      postMessage(ary);
+    }
+    this.elementCount = 0;
+    this.bufferIndex = 0;
   }
 }
