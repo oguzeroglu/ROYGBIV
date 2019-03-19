8d69c05504a4213d3ef639e68a6024b5249aea7b
oguz <eroglu.oguz93@gmail.com>
2019-03-19 22:47:11 +0100

    do not increment TOTAL_OBJECT_COLLISION_LISTENER_COUNT ctr for same objects

--- js/engine_objects/Roygbiv.js
+++ js/engine_objects/Roygbiv.js
@@ -3635,9 +3635,11 @@ Roygbiv.prototype.setCollisionListener = function(sourceObject, callbackFunction
   if ((sourceObject.isAddedObject) || (sourceObject.isObjectGroup)){
     preConditions.checkIfTrue(ROYGBIV.setCollisionListener, "Cannot set collision listener for more than "+MAX_OBJECT_COLLISION_LISTENER_COUNT+" objects.", (TOTAL_OBJECT_COLLISION_LISTENER_COUNT >= MAX_OBJECT_COLLISION_LISTENER_COUNT));
     preConditions.checkIfNoMass(ROYGBIV.setCollisionListener, preConditions.sourceObject, sourceObject);
+    if (!collisionCallbackRequests.has(sourceObject.name)){
+      TOTAL_OBJECT_COLLISION_LISTENER_COUNT ++;
+    }
     collisionCallbackRequests.set(sourceObject.name, callbackFunction.bind(sourceObject));
     physicsWorld.setCollisionListener(sourceObject);
-    TOTAL_OBJECT_COLLISION_LISTENER_COUNT ++;
   }else if (sourceObject.isParticle){
     preConditions.checkIfTrue(ROYGBIV.setCollisionListener, "Particle system is stopped.", (sourceObject.parent && sourceObject.parent.isStopped));
     if (sourceObject.uuid && !particleCollisionCallbackRequests[sourceObject.uuid]){
