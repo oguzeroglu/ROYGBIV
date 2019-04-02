aec5369f5f473f300464edd15da8451ba9826f30
oguz <eroglu.oguz93@gmail.com>
2019-04-02 21:19:13 +0200

    do not try to autoinstance objects with different texture properties

--- js/handler/AutoInstancingHandler.js
+++ js/handler/AutoInstancingHandler.js
@@ -2,26 +2,47 @@ var AutoInstancingHandler = function(){
   this.maxBatchObjectSize = parseInt((MAX_VERTEX_UNIFORM_VECTORS - 50) / 3);
 }
 
+AutoInstancingHandler.prototype.getObjectKey = function(obj){
+  var geomKey = obj.mesh.geometry.uuid;
+  var diffuseKey = "null", alphaKey = "null", aoKey = "null", displacementKey = "null", emissiveKey = "null";
+  if (obj.hasDiffuseMap()){
+    diffuseKey = obj.mesh.material.uniforms.diffuseMap.value.uuid;
+  }
+  if (obj.hasAlphaMap()){
+    alphaKey = obj.mesh.material.uniforms.alphaMap.value.uuid;
+  }
+  if (obj.hasAOMap()){
+    aoKey = obj.mesh.material.uniforms.aoMap.value.uuid;
+  }
+  if (obj.hasDisplacementMap()){
+    displacementKey = obj.mesh.material.uniforms.displacementMap.value.uuid;
+  }
+  if (obj.hasEmissiveMap()){
+    emissiveKey = obj.mesh.material.uniforms.emissiveMap.value.uuid;
+  }
+  return geomKey + PIPE + diffuseKey + PIPE + alphaKey + PIPE + aoKey + PIPE + displacementKey + PIPE + emissiveKey;
+}
+
 AutoInstancingHandler.prototype.handle = function(){
   if (!INSTANCING_SUPPORTED){
     return;
   }
   autoInstancedObjects = new Object();
   var objectsByGeometryID = new Object();
-  var countersByGeometryID = new Object();
+  var countersByObjectKey = new Object();
   for (var objName in addedObjects){
     var obj = addedObjects[objName];
     if (obj.isChangeable || (!obj.noMass && obj.physicsBody.mass > 0)){
-      var geom = obj.mesh.geometry;
-      if (typeof countersByGeometryID[geom.uuid] == UNDEFINED){
-        countersByGeometryID[geom.uuid] = 0;
+      var objKey = this.getObjectKey(obj);
+      if (typeof countersByObjectKey[objKey] == UNDEFINED){
+        countersByObjectKey[objKey] = 0;
       }
-      var key = geom.uuid + PIPE + countersByGeometryID[geom.uuid];
+      var key = objKey + PIPE + countersByObjectKey[objKey];
       if (!objectsByGeometryID[key]){
         objectsByGeometryID[key] = [];
       }else if (objectsByGeometryID[key].length > this.maxBatchObjectSize){
-        countersByGeometryID[geom.uuid] ++;
-        key = geom.uuid + PIPE + (countersByGeometryID[geom.uuid]);
+        countersByObjectKey[objKey] ++;
+        key = objKey + PIPE + (countersByObjectKey[objKey]);
         objectsByGeometryID[key] = [];
       }
       objectsByGeometryID[key].push(obj);
