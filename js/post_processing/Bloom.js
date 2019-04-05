11b2d5ec2bfe529e07b26ba20177365c943f74c4
oguz <eroglu.oguz93@gmail.com>
2019-04-06 00:10:16 +0200

    more bloom

--- js/post_processing/Bloom.js
+++ js/post_processing/Bloom.js
@@ -9,15 +9,20 @@ var Bloom = function(){
     fragmentShader: ShaderContent.bloomBlurFragmentShader,
     uniforms: {
       modelViewMatrix: new THREE.Uniform(), projectionMatrix: new THREE.Uniform(), sceneTexture: new THREE.Uniform(),
-      direction: new THREE.Uniform(new THREE.Vector2()), resolution: new THREE.Uniform(new THREE.Vector2())
+      direction: new THREE.Uniform(new THREE.Vector2()), resolution: new THREE.Uniform(new THREE.Vector2()),
+      brightnessThreshold: new THREE.Uniform(1), combineTexture: new THREE.Uniform(), combineFlag: new THREE.Uniform(-10.0)
     }
   });
   this.blurQuad = new THREE.Mesh(REUSABLE_QUAD_GEOMETRY, blurMaterial);
   blurMaterial.uniforms.projectionMatrix.value = orthographicCamera.projectionMatrix;
   blurMaterial.uniforms.modelViewMatrix.value = this.blurQuad.modelViewMatrix;
-  blurMaterial.uniforms.sceneTexture.value = this.sceneTarget.texture;
+  blurMaterial.uniforms.combineTexture.value = this.sceneTarget.texture;
   this.blurScene = new THREE.Scene();
   this.blurScene.add(this.blurQuad);
+}
+
+Bloom.prototype.setBrightnessThreshold = function(brightnessThreshold){
+  this.blurQuad.material.uniforms.brightnessThreshold.value = brightnessThreshold;
 }
 
 Bloom.prototype.setSize = function(width, height){
@@ -41,8 +46,10 @@ Bloom.prototype.render = function(){
   this.blurQuad.material.uniforms.direction.value.set(1, 0);
   this.blurQuad.material.uniforms.resolution.value.set(this.blurTarget.width / this.blurSizeCoef, this.blurTarget.height / this.blurSizeCoef);
   this.blurQuad.material.uniforms.sceneTexture.value = this.sceneTarget.texture;
+  this.blurQuad.material.uniforms.combineFlag.value = -10.0;
   renderer.webglRenderer.render(this.blurScene, orthographicCamera, this.blurTarget);
   this.blurQuad.material.uniforms.direction.value.set(0, 1);
+  this.blurQuad.material.uniforms.combineFlag.value = 10.0;
   this.blurQuad.material.uniforms.sceneTexture.value = this.blurTarget.texture;
   renderer.webglRenderer.render(this.blurScene, orthographicCamera);
 }
--- shader/post_processing/bloom/blur/fragmentShader.shader
+++ shader/post_processing/bloom/blur/fragmentShader.shader
@@ -2,16 +2,19 @@ precision lowp float;
 precision lowp int;
 
 uniform sampler2D sceneTexture;
+uniform sampler2D combineTexture;
 uniform vec2 direction;
 uniform vec2 resolution;
+uniform float brightnessThreshold;
+uniform float combineFlag;
 varying vec2 vUV;
 
-vec4 blur() {
+vec4 blur(vec4 color1) {
   vec4 color = vec4(0.0);
   vec2 off1 = vec2(1.411764705882353) * direction;
   vec2 off2 = vec2(3.2941176470588234) * direction;
   vec2 off3 = vec2(5.176470588235294) * direction;
-  color += texture2D(sceneTexture, vUV) * 0.1964825501511404;
+  color += color1 * 0.1964825501511404;
   color += texture2D(sceneTexture, vUV + (off1 / resolution)) * 0.2969069646728344;
   color += texture2D(sceneTexture, vUV - (off1 / resolution)) * 0.2969069646728344;
   color += texture2D(sceneTexture, vUV + (off2 / resolution)) * 0.09447039785044732;
@@ -22,5 +25,15 @@ vec4 blur() {
 }
 
 void main(){
-  gl_FragColor = blur();
+  vec4 color1 = texture2D(sceneTexture, vUV);
+  float brightness = dot(color1.rgb, vec3(0.2126, 0.7152, 0.0722));
+  if (brightness > brightnessThreshold){
+    gl_FragColor = blur(color1);
+  }else{
+    gl_FragColor = vec4(0.0);
+  }
+  if (combineFlag > 0.0){
+    vec4 sceneColor = texture2D(combineTexture, vUV);
+    gl_FragColor = gl_FragColor + sceneColor;
+  }
 }
