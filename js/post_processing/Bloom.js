57b51bc64ba49e954c0070751bdde55f423f850d
oguz <eroglu.oguz93@gmail.com>
2019-04-05 23:52:13 +0200

    more bloom stuff

--- js/post_processing/Bloom.js
+++ js/post_processing/Bloom.js
@@ -1,12 +1,16 @@
 var Bloom = function(){
   var rtParameters = {minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat};
-  this.blurTargetScaleCoef = 4;
+  this.blurSizeCoef = 2;
   this.sceneTarget = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, rtParameters);
-  this.blurTarget = new THREE.WebGLRenderTarget(window.innerWidth / this.blurTargetScaleCoef, window.innerHeight / this.blurTargetScaleCoef);
+  this.blurTarget = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
+  this.blurTarget2 = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
   var blurMaterial = new THREE.RawShaderMaterial({
     vertexShader: ShaderContent.bloomBlurVertexShader,
     fragmentShader: ShaderContent.bloomBlurFragmentShader,
-    uniforms: {modelViewMatrix: new THREE.Uniform(), projectionMatrix: new THREE.Uniform(), sceneTexture: new THREE.Uniform()}
+    uniforms: {
+      modelViewMatrix: new THREE.Uniform(), projectionMatrix: new THREE.Uniform(), sceneTexture: new THREE.Uniform(),
+      direction: new THREE.Uniform(new THREE.Vector2()), resolution: new THREE.Uniform(new THREE.Vector2())
+    }
   });
   this.blurQuad = new THREE.Mesh(REUSABLE_QUAD_GEOMETRY, blurMaterial);
   blurMaterial.uniforms.projectionMatrix.value = orthographicCamera.projectionMatrix;
@@ -18,12 +22,14 @@ var Bloom = function(){
 
 Bloom.prototype.setSize = function(width, height){
   this.sceneTarget.setSize(width, height);
-  this.blurTarget.setSize(width / this.blurTargetScaleCoef, height / this.blurTargetScaleCoef);
+  this.blurTarget.setSize(width, height);
+  this.blurTarget2.setSize(width, height);
 }
 
 Bloom.prototype.setViewport = function(x, y, z, w){
   this.sceneTarget.viewport.set(x, y, z, w);
-  this.blurTarget.viewport.set(x, y, z / this.blurTargetScaleCoef, w / this.blurTargetScaleCoef);
+  this.blurTarget.viewport.set(x, y, z, w);
+  this.blurTarget2.viewport.set(x, y, z, w);
 }
 
 Bloom.prototype.setPixelRatio = function(ratio){
@@ -32,5 +38,11 @@ Bloom.prototype.setPixelRatio = function(ratio){
 
 Bloom.prototype.render = function(){
   renderer.webglRenderer.render(scene, camera, this.sceneTarget);
+  this.blurQuad.material.uniforms.direction.value.set(1, 0);
+  this.blurQuad.material.uniforms.resolution.value.set(this.blurTarget.width / this.blurSizeCoef, this.blurTarget.height / this.blurSizeCoef);
+  this.blurQuad.material.uniforms.sceneTexture.value = this.sceneTarget.texture;
   renderer.webglRenderer.render(this.blurScene, orthographicCamera, this.blurTarget);
+  this.blurQuad.material.uniforms.direction.value.set(0, 1);
+  this.blurQuad.material.uniforms.sceneTexture.value = this.blurTarget.texture;
+  renderer.webglRenderer.render(this.blurScene, orthographicCamera);
 }
--- shader/post_processing/bloom/blur/fragmentShader.shader
+++ shader/post_processing/bloom/blur/fragmentShader.shader
@@ -2,8 +2,25 @@ precision lowp float;
 precision lowp int;
 
 uniform sampler2D sceneTexture;
+uniform vec2 direction;
+uniform vec2 resolution;
 varying vec2 vUV;
 
+vec4 blur() {
+  vec4 color = vec4(0.0);
+  vec2 off1 = vec2(1.411764705882353) * direction;
+  vec2 off2 = vec2(3.2941176470588234) * direction;
+  vec2 off3 = vec2(5.176470588235294) * direction;
+  color += texture2D(sceneTexture, vUV) * 0.1964825501511404;
+  color += texture2D(sceneTexture, vUV + (off1 / resolution)) * 0.2969069646728344;
+  color += texture2D(sceneTexture, vUV - (off1 / resolution)) * 0.2969069646728344;
+  color += texture2D(sceneTexture, vUV + (off2 / resolution)) * 0.09447039785044732;
+  color += texture2D(sceneTexture, vUV - (off2 / resolution)) * 0.09447039785044732;
+  color += texture2D(sceneTexture, vUV + (off3 / resolution)) * 0.010381362401148057;
+  color += texture2D(sceneTexture, vUV - (off3 / resolution)) * 0.010381362401148057;
+  return color;
+}
+
 void main(){
-  gl_FragColor = texture2D(sceneTexture, vUV);
+  gl_FragColor = blur();
 }
