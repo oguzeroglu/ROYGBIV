7cd5429b83fa27f092ff653091e28b80ef5137a0
oguz <eroglu.oguz93@gmail.com>
2019-04-28 09:34:23 +0200

    deployment mode particleSystemCreationGUIHandler update fix

--- js/core/renderUtils.js
+++ js/core/renderUtils.js
@@ -34,7 +34,9 @@ function render(){
     cpuOperationsHandler.updateCrosshair();
     cpuOperationsHandler.handleObjectMouseEvents();
   }else{
-    particleSystemCreatorGUIHandler.update();
+    if (!isDeployment){
+      particleSystemCreatorGUIHandler.update();
+    }
     cameraOperationsDone = false;
   }
 
