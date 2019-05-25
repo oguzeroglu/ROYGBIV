614e0a8c8857e7715085db2afecbc2ab2437849a
oguz <eroglu.oguz93@gmail.com>
2019-05-25 12:43:36 +0200

    TexturePackCreatorGUIHandler test mesh handling

--- js/handler/gui/GUIHandler.js
+++ js/handler/gui/GUIHandler.js
@@ -110,7 +110,7 @@ var GUIHandler = function(){
   }
   // GUI TYPES DEFINITION
   this.guiTypes = {
-    FOG: 0, SKYBOX: 1, TEXT: 2, OBJECT: 3, BLOOM: 4, FPS_WEAPON_ALIGNMENT: 5, SHADER_PRECISION: 6, PARTICLE_SYSTEM: 7, WORKER_STATUS: 8, MUZZLE_FLASH: 9
+    FOG: 0, SKYBOX: 1, TEXT: 2, OBJECT: 3, BLOOM: 4, FPS_WEAPON_ALIGNMENT: 5, SHADER_PRECISION: 6, PARTICLE_SYSTEM: 7, WORKER_STATUS: 8, MUZZLE_FLASH: 9, TEXTURE_PACK: 10
   };
 }
 
@@ -620,6 +620,12 @@ GUIHandler.prototype.hide = function(guiType){
       if (this.datGuiMuzzleFlashCreator){
         this.destroyGUI(this.datGuiMuzzleFlashCreator);
         this.datGuiMuzzleFlashCreator = 0;
+      }
+    return;
+    case this.guiTypes.TEXTURE_PACK:
+      if (this.datGuiTexturePack){
+        this.destroyGUI(this.datGuiTexturePack);
+        this.datGuiTexturePack = 0;
       }
     return;
   }
--- js/handler/gui/TexturePackCreatorGUIHandler.js
+++ js/handler/gui/TexturePackCreatorGUIHandler.js
@@ -1,7 +1,80 @@
 var TexturePackCreatorGUIHandler = function(){
+  this.testTypes = ["BOX", "SPHERE", "SURFACE", "CYLINDER"];
+}
 
+TexturePackCreatorGUIHandler.prototype.init = function(){
+  this.configurations = {
+    "Texture pack": "",
+    "Test type": "BOX",
+    "Done": function(){
+
+    },
+    "Cancel": function(){
+
+    }
+  }
+}
+
+TexturePackCreatorGUIHandler.prototype.disposeTestMesh = function(){
+  this.testMesh.geometry.dispose();
+  if (!(typeof this.testMesh.material.uniforms.diffuseMap == UNDEFINED)){
+    this.testMesh.material.uniforms.diffuseMap.dispose();
+  }
+  if (!(typeof this.testMesh.material.uniforms.alphaMap == UNDEFINED)){
+    this.testMesh.material.uniforms.alphaMap.dispose();
+  }
+  if (!(typeof this.testMesh.material.uniforms.aoMap == UNDEFINED)){
+    this.testMesh.material.uniforms.aoMap.dispose();
+  }
+  if (!(typeof this.testMesh.material.uniforms.emissiveMap == UNDEFINED)){
+    this.testMesh.material.uniforms.emissiveMap.dispose();
+  }
+  if (!(typeof this.testMesh.material.uniforms.displacementMap == UNDEFINED)){
+    this.testMesh.material.uniforms.displacementMap.dispose();
+  }
+  this.testMesh.material.dispose();
+}
+
+TexturePackCreatorGUIHandler.prototype.handleTestMesh = function(){
+  if (this.testMesh){
+    scene.remove(this.testMesh);
+    this.disposeTestMesh();
+    this.testMesh = 0;
+  }
+  var geometry;
+  switch(this.configurations["Test type"]){
+    case "BOX": geometry = new THREE.BoxBufferGeometry(50, 50, 50); break;
+    case "SPHERE": geometry = new THREE.SphereBufferGeometry(25); break;
+    case "SURFACE": geometry = new THREE.PlaneBufferGeometry(50, 50); break;
+    case "CYLINDER": geometry = new THREE.CylinderBufferGeometry(25, 25, 20); break;
+    default: throw new Error("Type not supported."); break;
+  }
+  this.testMesh = new MeshGenerator(geometry, new BasicMaterial({name: "test", color: "#ffffff", alpha: 1})).generateMesh();
+  scene.add(this.testMesh);
 }
 
 TexturePackCreatorGUIHandler.prototype.show = function(texturePackName, folders){
-  
+  this.init();
+  selectionHandler.resetCurrentSelection();
+  guiHandler.hideAll();
+  this.hiddenEngineObjects = [];
+  for (var i = 0; i<scene.children.length; i++){
+    var child = scene.children[i];
+    if (child.visible){
+      child.visible = false;
+      this.hiddenEngineObjects.push(child);
+    }
+  }
+  activeControl = new OrbitControls({maxRadius: 500, zoomDelta: 5});
+  activeControl.onActivated();
+  guiHandler.datGuiTexturePack = new dat.GUI({hideable: false});
+  guiHandler.datGuiTexturePack.add(this.configurations, "Texture pack", folders).onChange(function(val){
+
+  }).listen();
+  guiHandler.datGuiTexturePack.add(this.configurations, "Test type", this.testTypes).onChange(function(val){
+    texturePackCreatorGUIHandler.handleTestMesh();
+  }).listen();
+  guiHandler.datGuiTexturePack.add(this.configurations, "Cancel");
+  guiHandler.datGuiTexturePack.add(this.configurations, "Done");
+  this.handleTestMesh();
 }
