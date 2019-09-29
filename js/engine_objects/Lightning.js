6611dccd1e5326440ca3af6c9953aac04f5f4277
oguz <eroglu.oguz93@gmail.com>
2019-09-29 15:30:20 +0200

    refactor

--- js/engine_objects/Lightning.js
+++ js/engine_objects/Lightning.js
@@ -1,11 +1,20 @@
-var Lightning = function(){
-  this.DETAIL_THRESHOLD = 2;
+var Lightning = function(name, detailThreshold, maxDisplacement, count){
+  this.name = name;
+  this.detailThreshold = detailThreshold;
+  this.maxDisplacement = maxDisplacement;
+  this.count = count;
   this.tree = new Object();
   this.renderMap = new Object();
   this.idCounter = 0;
 }
 
-Lightning.prototype.addSegment = function(node, startPoint, endPoint){
+Lightning.prototype.init = function(startPoint, endPoint){
+  for (var i = 0; i<this.count; i++){
+    this.generateTree(this.tree, startPoint, endPoint, this.maxDisplacement, false);
+  }
+}
+
+Lightning.prototype.addSegment = function(node, startPoint, endPoint, isBranch){
   node[this.idCounter ++] = {
     id: this.idCounter -1,
     startPoint: startPoint,
@@ -15,25 +24,24 @@ Lightning.prototype.addSegment = function(node, startPoint, endPoint){
   return node[this.idCounter - 1];
 }
 
-Lightning.prototype.test = function(node, startPoint, endPoint, displacement){
+Lightning.prototype.generateTree = function(node, startPoint, endPoint, displacement){
   var addedNode = this.addSegment(node, startPoint, endPoint, displacement);
   var children = addedNode.children;
-  if (displacement > this.DETAIL_THRESHOLD){
+  if (displacement > this.detailThreshold){
     displacement = displacement / 2;
     var middlePoint = new THREE.Vector3(
       ((startPoint.x + endPoint.x) / 2) + displacement * (Math.random() - 0.5),
       ((startPoint.y + endPoint.y) / 2) + displacement * (Math.random() - 0.5),
       ((startPoint.z + endPoint.z) / 2) + displacement * (Math.random() - 0.5)
     );
-    this.test(children, startPoint, middlePoint, displacement);
-    this.test(children, middlePoint, endPoint, displacement);
+    this.generateTree(children, startPoint, middlePoint, displacement);
+    this.generateTree(children, middlePoint, endPoint, displacement);
   }else{
     this.renderMap[addedNode.id] = addedNode;
   }
 }
 
 Lightning.prototype.debug = function(){
-  // DEBUG
   var material = new THREE.LineBasicMaterial( { color: "lime" } );
   var geom = new THREE.Geometry();
   for (var id in this.renderMap){
