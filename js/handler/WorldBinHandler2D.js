d0e8feba6b809beb93ae47c5b6ef7feb004bc6f9
oguz <eroglu.oguz93@gmail.com>
2019-10-30 14:26:46 +0100

    WorldBinHandler2D first commit

---
+++ js/handler/WorldBinHandler2D.js
@@ -0,0 +1,45 @@
+var WorldBinHandler2D = function(floatingPointCount){
+  this.bin = new Map();
+  this.floatingPointCount = floatingPointCount; // number of digits after floating point
+  this.segmentCount = Math.pow(10, this.floatingPointCount);
+}
+
+WorldBinHandler2D.prototype.convertFromWebGLRange = function(value){
+  return (value +1) / 2;
+}
+
+WorldBinHandler2D.prototype.getBinIndex = function(float){
+  var result = (float.toFixed(this.floatingPointCount) * this.segmentCount) - 1;
+  if (result < 0){
+    result = 0;
+  }
+  return result;
+}
+
+WorldBinHandler2D.prototype.insertAddedText = function(obj){
+  if (!obj.twoDimensionalSize){
+    obj.handleResize();
+  }
+  var minXIndex = this.getBinIndex(this.convertFromWebGLRange(obj.twoDimensionalSize.x));
+  var maxYIndex = this.getBinIndex(this.convertFromWebGLRange(obj.twoDimensionalSize.y));
+  var maxXIndex = this.getBinIndex(this.convertFromWebGLRange(obj.twoDimensionalSize.z));
+  var minYIndex = this.getBinIndex(this.convertFromWebGLRange(obj.twoDimensionalSize.w));
+  for (var x = minXIndex; x<= maxXIndex; x++){
+    for (var y = minYIndex; y<= maxYIndex; y++){
+      if (!this.bin.has(x)){
+        this.bin.set(x, new Map());
+      }
+      if (!this.bin.get(x).has(y)){
+        this.bin.get(x).set(y, new Map());
+      }
+      this.bin.get(x).get(y).set(obj.name, true);
+    }
+  }
+}
+
+WorldBinHandler2D.prototype.insert = function(obj){
+  if (obj.isAddedText){
+    this.insertAddedText(obj);
+    return;
+  }
+}
