b2a0d4fc855586fb6e872680c9ba00e96a6bb157
Oguz <eroglu.oguz93@gmail.com>
2020-03-28 06:30:47 +0100

    removeAmbientLight impl

--- js/handler/LightHandler.js
+++ js/handler/LightHandler.js
@@ -2,6 +2,22 @@ var LightHandler = function(){
 
 }
 
+LightHandler.prototype.removeAmbientLight = function(){
+  var addedObjectsInScene = sceneHandler.getAddedObjects();
+
+  for (var objName in addedObjectsInScene){
+    var obj = addedObjectsInScene[objName];
+    if (!obj.affectedByLight){
+      continue;
+    }
+
+    this.removeStaticAmbientLightMacros(obj);
+  }
+
+  delete this.ambientColor;
+  delete this.ambientStrength;
+}
+
 LightHandler.prototype.setAmbientLight = function(color, strength){
   var addedObjectsInScene = sceneHandler.getAddedObjects();
 
@@ -20,8 +36,7 @@ LightHandler.prototype.setAmbientLight = function(color, strength){
   return this.ambientLightCount;
 }
 
-LightHandler.prototype.handleStaticAmbientLightMacros = function(obj, color, strength) {
-
+LightHandler.prototype.removeStaticAmbientLightMacros = function(obj){
   if (this.ambientColor){
     macroHandler.removeMacro("AFFECTED_BY_LIGHT", obj.mesh.material, true, false);
     macroHandler.removeMacro("STATIC_AMBIENT_LIGHT_R " + this.ambientColor.r, obj.mesh.material, true, false);
@@ -30,6 +45,11 @@ LightHandler.prototype.handleStaticAmbientLightMacros = function(obj, color, str
     macroHandler.removeMacro("STATIC_AMBIENT_LIGHT_STRENGTH " + this.ambientStrength, obj.mesh.material, true, false);
     macroHandler.removeMacro("HAS_STATIC_AMBIENT_LIGHT", obj.mesh.material, true, false);
   }
+}
+
+LightHandler.prototype.handleStaticAmbientLightMacros = function(obj, color, strength){
+
+  this.removeStaticAmbientLightMacros(obj);
 
   macroHandler.injectMacro("AFFECTED_BY_LIGHT", obj.mesh.material, true, false);
   macroHandler.injectMacro("STATIC_AMBIENT_LIGHT_R " + color.r, obj.mesh.material, true, false);
