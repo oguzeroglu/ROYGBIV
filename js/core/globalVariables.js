f9e38d031b584c8d1f212798c51948924351acd6
Oguz <eroglu.oguz93@gmail.com>
2020-08-18 18:02:57 +0200

    add block ascii to supported chars

--- js/core/globalVariables.js
+++ js/core/globalVariables.js
@@ -519,7 +519,7 @@ var supportedFontAtlasChars = [
   "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
   "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`",
   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-  "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~", "q", "Q"
+  "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~", "q", "Q", "█"
 ];
 var supportedFontCharMap = new Object();
 for (var i = 0; i<supportedFontAtlasChars.length; i++){
