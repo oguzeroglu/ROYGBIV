84fca8adf1060b49677de2c60984bfb94063f2ae
Oguz <eroglu.oguz93@gmail.com>
2020-09-07 21:37:22 +0200

    DecisionTreeCreatorGUIHandler visualiseDecisionTree API impl

--- js/handler/gui/DecisionTreeCreatorGUIHandler.js
+++ js/handler/gui/DecisionTreeCreatorGUIHandler.js
@@ -38,9 +38,60 @@ DecisionTreeCreatorGUIHandler.prototype.createMermaidContainer = function(){
   mermaidContainer.style.width = "100%";
   mermaidContainer.style.height = "100%";
   mermaidContainer.style.backgroundColor = "#d3d3d3";
+  mermaidContainer.style.overflowX = "scroll";
+  mermaidContainer.style.overflowY = "scroll";
   mermaidContainer.className = "mermaid";
 
   document.body.prepend(mermaidContainer);
 
   this.mermaidContainer = mermaidContainer;
+}
+
+DecisionTreeCreatorGUIHandler.prototype.getDecisionMermaidText = function(preconfiguredDecision, id){
+  var decisionName = preconfiguredDecision.decisionName;
+  var uuid = id || generateUUID();
+  var nameText = uuid + "{" + decisionName + "}";
+
+  var text = nameText + "\n";
+
+  if (preconfiguredDecision.yesNode != null){
+    var yesNodeID = generateUUID();
+    if (preconfiguredDecision.yesNode instanceof PreconfiguredDecision){
+      var yesNodeText = yesNodeID + "{" + preconfiguredDecision.yesNode.decisionName + "}";
+      text += nameText + " -->|Yes| " + yesNodeText + "\n";
+      text += this.getDecisionMermaidText(preconfiguredDecision.yesNode, yesNodeID);
+    }else{
+      var yesNodeText = yesNodeID + "{" + preconfiguredDecision.yesNode + "}";
+      text += nameText + " -->|Yes| " + yesNodeText + "\n";
+    }
+  }
+
+  if (preconfiguredDecision.noNode != null){
+    var noNodeID = generateUUID();
+    if (preconfiguredDecision.noNode instanceof PreconfiguredDecision){
+      var noNodeText = noNodeID + "{" + preconfiguredDecision.noNode.decisionName + "}";
+      text += nameText + " -->|No| " + noNodeText + "\n";
+      text += this.getDecisionMermaidText(preconfiguredDecision.noNode, noNodeID);
+    }else{
+      var noNodeText = noNodeID + "{" + preconfiguredDecision.noNode + "}";
+      text += nameText + " -->|No| " + noNodeText + "\n";
+    }
+  }
+
+  return text;
+}
+
+DecisionTreeCreatorGUIHandler.prototype.visualiseDecisionTree = function(preconfiguredDecisionTree){
+  if (!preconfiguredDecisionTree.rootDecision){
+    return false;
+  }
+
+  var mermaidText = "graph TD\n";
+
+  mermaidText += this.getDecisionMermaidText(preconfiguredDecisionTree.rootDecision);
+
+  this.mermaidContainer.innerHTML = mermaidText;
+  this.mermaidContainer.removeAttribute("data-processed");
+
+  mermaid.init();
 }
