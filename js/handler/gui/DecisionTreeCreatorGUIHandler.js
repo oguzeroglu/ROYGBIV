f5ab481008d77192b512ff8b213bec6b350e481f
Oguz <eroglu.oguz93@gmail.com>
2020-09-07 20:37:25 +0200

    create mermaid container

--- js/handler/gui/DecisionTreeCreatorGUIHandler.js
+++ js/handler/gui/DecisionTreeCreatorGUIHandler.js
@@ -10,5 +10,37 @@ DecisionTreeCreatorGUIHandler.prototype.show = function(){
   selectionHandler.resetCurrentSelection();
   guiHandler.hideAll();
 
-  guiHandler.datGuiDecisionCreation = new dat.GUI({hideable: false});
+  this.createMermaidContainer();
+
+  guiHandler.datGuiDecisionTreeCreation = new dat.GUI({hideable: false});
+}
+
+DecisionTreeCreatorGUIHandler.prototype.hide = function(){
+  document.body.removeChild(this.mermaidContainer);
+  delete this.mermaidContainer;
+
+  canvas.style.visibility = "";
+
+  terminal.clear();
+  terminal.enable();
+  guiHandler.hide(guiHandler.guiTypes.DECISION_TREE_CREATION);
+  terminal.printInfo(Text.GUI_CLOSED);
+}
+
+DecisionTreeCreatorGUIHandler.prototype.createMermaidContainer = function(){
+  canvas.style.visibility = "hidden";
+
+  var mermaidContainer = document.createElement("div");
+  mermaidContainer.style.display = "block";
+  mermaidContainer.style.position = "absolute";
+  mermaidContainer.style.top = "0";
+  mermaidContainer.style.left = "0";
+  mermaidContainer.style.width = "100%";
+  mermaidContainer.style.height = "100%";
+  mermaidContainer.style.backgroundColor = "#d3d3d3";
+  mermaidContainer.className = "mermaid";
+
+  document.body.prepend(mermaidContainer);
+
+  this.mermaidContainer = mermaidContainer;
 }
