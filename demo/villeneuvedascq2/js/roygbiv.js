dc8726795983125a15a229dc772c812ae28f5a36
oguzeroglu <eroglu.oguz93@gmail.com>
2020-11-01 13:10:56 +0100

    turn off antialias from villeneuvedascq demo

--- demo/villeneuvedascq2/js/roygbiv.js
+++ demo/villeneuvedascq2/js/roygbiv.js
@@ -1298,7 +1298,7 @@ var jobHandlerShadowBakerRefresh = false;
 var geometryCache = new Object();
 var physicsShapeCache = new Object();
 var MAX_TEXTURE_COUNT = 8;
-var ENABLE_ANTIALIAS = true;
+var ENABLE_ANTIALIAS = false;
 var compressedTextureFallbackFormat = ".png";
 var webglCallbackHandler;
 var threejsRenderMonitoringHandler;
