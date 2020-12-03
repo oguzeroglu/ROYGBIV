342a6f9c1e13ec963dce463e5f040172e6eb730e
oguzeroglu <eroglu.oguz93@gmail.com>
2020-12-03 23:36:04 +0100

    ModelCreatorGUIHandler second commit

--- js/handler/gui/ModelCreatorGUIHandler.js
+++ js/handler/gui/ModelCreatorGUIHandler.js
@@ -31,6 +31,8 @@ ModelCreatorGUIHandler.prototype.show = function(){
         modelCreatorGUIHandler.close(Text.NO_VALID_MODELS_UNDER_MODELS_FOLDER, true);
         return;
       }
+
+      modelCreatorGUIHandler.renderControls(resp, 0);
     }
   }
   xhr.send();
@@ -55,4 +57,56 @@ ModelCreatorGUIHandler.prototype.close = function(message, isError){
   activeControl.onActivated();
   camera.quaternion.set(0, 0, 0, 1);
   camera.position.set(initialCameraX, initialCameraY, initialCameraZ);
+}
+
+ModelCreatorGUIHandler.prototype.renderControls = function(allModels, index){
+  terminal.clear();
+  terminal.printInfo(Text.LOADING_MODEL);
+
+  var modelToLoad = allModels[index];
+  modelLoader.loadModel(modelToLoad.folder, modelToLoad.obj, modelToLoad.mtl, function(model){
+    terminal.clear();
+    terminal.printInfo(Text.MODEL_LOADED);
+
+    if (guiHandler.datGuiModelCreation){
+      guiHandler.removeControllers(guiHandler.datGuiModelCreation);
+    }else{
+      guiHandler.datGuiModelCreation = new dat.GUI({hideable: false});
+    }
+
+    var allFolders = [];
+    for (var i = 0; i < allModels.length; i ++){
+      allFolders.push(allModels[i].folder);
+    }
+
+    var childrenNames = [];
+    for (var i = 0; i < model.children.length; i ++){
+      var child = model.children[i];
+      childrenNames.push(child.name || "Child" + i);
+    }
+
+    var params = {
+      "Folder": allFolders[index],
+      "Child": childrenNames[0],
+      "Done": function(){
+
+      },
+      "Cancel": function(){
+        modelCreatorGUIHandler.close(Text.OPERATION_CANCELLED, false);
+      }
+    };
+
+    var folderController, childController;
+
+    folderController = guiHandler.datGuiModelCreation.add(params, "Folder", allFolders).onChange(function(val){
+      guiHandler.disableController(folderController);
+      guiHandler.disableController(childController);
+      modelCreatorGUIHandler.renderControls(allModels, allFolders.indexOf(val));
+    });
+    childController = guiHandler.datGuiModelCreation.add(params, "Child", childrenNames);
+    guiHandler.datGuiModelCreation.add(params, "Done");
+    guiHandler.datGuiModelCreation.add(params, "Cancel");
+  }, function(){
+    modelCreatorGUIHandler.close(Text.ERROR_HAPPENED_LOADING_MODEL_FROM_FOLDER.replace(Text.PARAM1, modelToLoad.folder), true);
+  });
 }
