ea6c62a28d3fb1a26780bf75ed49287007464e5f
oguzeroglu <eroglu.oguz93@gmail.com>
2020-12-10 14:26:45 +0100

    improve RMF

--- js/handler/RMFHandler.js
+++ js/handler/RMFHandler.js
@@ -3,95 +3,55 @@ var RMFHandler = function(){
 }
 
 RMFHandler.prototype.generate = function(positions, normals, uvs){
-  var positionsPart = this.generatePart(positions, 3);
-  var normalsPart = this.generatePart(normals, 3);
-  var uvsPart = this.generatePart(uvs, 2);
+  var indexInfos = {};
+  var indexInfosInverse = {};
+  var curIndex = 0;
 
-  var total = new Float32Array(positionsPart.length + normalsPart.length + uvsPart.length);
-  total.set(positionsPart);
-  total.set(normalsPart, positionsPart.length);
-  total.set(uvsPart, normalsPart.length + positionsPart.length);
-
-  return total;
-}
-
-RMFHandler.prototype.generatePart = function(ary, incr){
-  var partMap = {};
-  var partMapInverse = {};
-  var partIndex = 0;
-  var partIndices = [];
-
-  for (var i = 0; i < ary.length; i += incr){
-    var curX, curY, curZ, curKey;
-    if (incr == 3){
-      curX = ary[i];
-      curY = ary[i + 1];
-      curZ = ary[i + 2];
-      curKey = curX + PIPE + curY + PIPE + curZ;
+  var i2 = 0;
+  var indices = [];
+  for (var i = 0; i < positions.length; i += 3){
+    var curPosX = positions[i];
+    var curPosY = positions[i + 1];
+    var curPosZ = positions[i + 2];
+    var curNormalX = normals[i];
+    var curNormalY = normals[i + 1];
+    var curNormalZ = normals[i + 2];
+    var curUVX = uvs[i2];
+    var curUVY = uvs[i2 + 1];
+    var key = curPosX + PIPE + curPosY + PIPE + curPosZ;
+    key += PIPE + curNormalX + PIPE + curNormalY + PIPE + curNormalZ;
+    key += PIPE + curUVX + PIPE + curUVY;
+    if (indexInfos[key]){
+      indices.push(indexInfos[key]);
     }else{
-      curX = ary[i];
-      curY = ary[i + 1];
-      curKey = curX + PIPE + curY;
+      indexInfos[key] = curIndex;
+      indexInfosInverse[curIndex] = key;
+      indices.push(curIndex);
+      curIndex ++;
     }
-    if (partMap[curKey]){
-      partIndices.push(partMap[curKey]);
-    }else{
-      partIndices.push(partIndex);
-      partMapInverse[partIndex] = curKey;
-      partMap[curKey] = partIndex ++;
-    }
+    i2 += 2;
   }
 
-  var allData = [];
-  for (var i = 0; i < partIndex; i ++){
-    var splitted = partMapInverse[i].split(PIPE);
-    allData.push(parseFloat(splitted[0]));
-    allData.push(parseFloat(splitted[1]));
-    if (incr == 3){
-      allData.push(parseFloat(splitted[2]));
-    }
+  var info = [curIndex];
+
+  for (var i = 0; i < curIndex; i ++){
+    var key = indexInfosInverse[i];
+    var splitted = key.split(PIPE);
+    info.push(parseFloat(splitted[0]));
+    info.push(parseFloat(splitted[1]));
+    info.push(parseFloat(splitted[2]));
+    info.push(parseFloat(splitted[3]));
+    info.push(parseFloat(splitted[4]));
+    info.push(parseFloat(splitted[5]));
+    info.push(parseFloat(splitted[6]));
+    info.push(parseFloat(splitted[7]));
   }
 
-  var partData = new Float32Array(1 + allData.length + 1 + partIndices.length);
-  var i = 0;
-  partData[i ++] = allData.length;
-  for (var i2 = 0; i2 < allData.length; i2 ++){
-    partData[i ++] = allData[i2];
-  }
-  partData[i ++] = partIndices.length;
-  for (var i2 = 0; i2 < partIndices.length; i2 ++){
-    partData[i ++] = partIndices[i2];
+  for (var i = 0; i < indices.length; i ++){
+    info.push(indices[i]);
   }
 
-  return partData;
-}
-
-RMFHandler.prototype.read = function(view, ary, count, startIndex){
-  var all = [];
-
-  for (var i = 0; i < view[startIndex]; i += count){
-    var x = view[startIndex + 1 + i];
-    var y = view[startIndex + 2 + i];
-    if (count == 3){
-      var z = view[startIndex + 3 + i];
-      all.push({x: x, y: y, z: z});
-    }else{
-      all.push({x: x, y: y});
-    }
-  }
-
-  var indicesLength = view[startIndex + view[startIndex] + 1];
-
-  for (var i = startIndex + view[startIndex] + 2; i < startIndex + view[startIndex] + 2 + indicesLength; i ++){
-    var data = all[view[i]];
-    ary.push(data.x);
-    ary.push(data.y);
-    if (count == 3){
-      ary.push(data.z);
-    }
-  }
-
-  return startIndex + view[startIndex] + 1 + indicesLength + 1;
+  return new Float32Array(info);
 }
 
 RMFHandler.prototype.load = function(folderName, onReady){
@@ -103,10 +63,48 @@ RMFHandler.prototype.load = function(folderName, onReady){
     var arrayBuffer = xhr.response;
     var view = new Float32Array(arrayBuffer);
 
-    var positions = [], normals = [], uvs = [];
+    var indicesLength = view[0];
+    var positionsByIndex = {};
+    var normalsByIndex = {};
+    var uvsByIndex = {};
 
-    // Black magic
-    rmfHandler.read(view, uvs, 2, rmfHandler.read(view, normals, 3, rmfHandler.read(view, positions, 3, 0)));
+    var positions = [];
+    var normals = [];
+    var uvs = [];
+
+    var totalRead = 0;
+    var i = 1;
+    while (totalRead != indicesLength){
+      var curPosX = view[i ++];
+      var curPosY = view[i ++];
+      var curPosZ = view[i ++];
+      var curNormalX = view[i ++];
+      var curNormalY = view[i ++];
+      var curNormalZ = view[i ++];
+      var curUVX = view[i ++];
+      var curUVY = view[i ++];
+
+      positionsByIndex[totalRead] = {x: curPosX, y: curPosY, z: curPosZ};
+      normalsByIndex[totalRead] = {x: curNormalX, y: curNormalY, z: curNormalZ};
+      uvsByIndex[totalRead] = {x: curUVX, y: curUVY};
+
+      totalRead ++;
+    }
+
+    for (var i = 1 + (indicesLength * 8); i < view.length; i ++){
+      var curIndex = view[i];
+      var pos = positionsByIndex[curIndex];
+      var normal = normalsByIndex[curIndex];
+      var uv = uvsByIndex[curIndex];
+      positions.push(pos.x);
+      positions.push(pos.y);
+      positions.push(pos.z);
+      normals.push(normal.x);
+      normals.push(normal.y);
+      normals.push(normal.z);
+      uvs.push(uv.x);
+      uvs.push(uv.y);
+    }
 
     onReady(positions, normals, uvs);
   };
