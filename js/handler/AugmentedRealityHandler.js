c178eab82def8a220d2a2f6395f2e66b9385b3a4
oguzeroglu <eroglu.oguz93@gmail.com>
2020-12-27 09:58:52 +0100

    isARSupported and startAugmentedReality scripting APIs

--- js/handler/AugmentedRealityHandler.js
+++ js/handler/AugmentedRealityHandler.js
@@ -2,8 +2,26 @@ var AugmentedRealityHandler = function(){
   var tmpAElement = document.createElement("a");
   this.isQuickLookSupported = tmpAElement.relList && tmpAElement.relList.supports && tmpAElement.relList.supports('ar');
   this.isScreenViewerSupported = isAndroid && !isFirefox && !isOcculus;
+
+  this.aElement = document.createElement("a");
 }
 
 AugmentedRealityHandler.prototype.isSupported = function(){
   return this.isQuickLookSupported || this.isScreenViewerSupported;
+}
+
+AugmentedRealityHandler.prototype.start = function(modelFolderName, arModelName){
+  if (!this.isSupported()){
+    return;
+  }
+
+  if (this.isQuickLookSupported){
+    this.aElement.setAttribute("rel", "ar");
+    var img = document.createElement("img");
+    this.aElement.appendChild(img);
+    this.aElement.setAttribute("href", "./models/" + modelFolderName + "/ar/" + arModelName + ".usdz");
+    this.aElement.click();
+    this.aElement.removeChild(img);
+    return;
+  }
 }
